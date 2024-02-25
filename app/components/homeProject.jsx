0c08f9141093084484e4e3c51083da5caeb8a062
Damion Morgan <only4school5680@gmail.com>
2024-02-25 11:49:14 -0500

    fixed the name

--- app/components/homeProject.jsx
+++ app/components/homeProject.jsx
@@ -2,6 +2,124 @@
 
 export default function Home() {
   return (
-    <div>homeProject</div>
+
+<section>
+  {/* <!-- Container --> */}
+  <div className="mx-auto w-full max-w-5xl px-5 py-16 md:px-10 md:py-24 lg:py-32">
+    {/* <!--  Component --> */}
+    <div className="flex flex-col items-stretch">
+      {/* <!--  Title --> */}
+      <h2 className="mb-5 text-center text-3xl font-bold md:text-5xl">Short heading goes here</h2>
+      <p className="mb-20 text-center text-sm sm:text-base">Lorem ipsum dolor sit amet, consectetur adipiscing</p>
+      {/* <!--  Content --> */}
+      <div className="gap-x-8 [column-count:1] md:grid-cols-2 md:gap-x-4 md:[column-count:2]">
+        {/* <!--  Item --> */}
+        <div className="mb-12 inline-block border border-solid border-[#cdcdcd] md:mb-8 lg:mb-10">
+          <img src="https://assets.website-files.com/624380709031623bfe4aee60/6243807090316262904aee69_Placeholder%20Image%20-%20Landscape.svg" alt="" className="inline-block" />
+          <div className="px-5 py-8 sm:px-6">
+            <h5 className="mb-3 text-xl font-bold">Project name here</h5>
+            <p className="flex-col text-[#808080]">Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna</p>
+            <div className="mb-5 mt-6 flex flex-wrap gap-2 md:mb-6 lg:mb-8">
+              <div className="rounded-sm bg-[#d9d9d9] p-2 text-sm font-semibold uppercase text-[#636262]">
+                <p>WEB DESIGN</p>
+              </div>
+              <div className="rounded-sm bg-[#d9d9d9] p-2 text-sm font-semibold uppercase text-[#636262]">
+                <p>UI / UX</p>
+              </div>
+              <div className="rounded-sm bg-[#d9d9d9] p-2 text-sm font-semibold uppercase text-[#636262]">
+                <p>WEBFLOW</p>
+              </div>
+            </div>
+            <div className="flex flex-wrap items-center justify-between gap-4">
+              <a href="#" className="r flex max-w-full gap-2.5 text-sm font-bold uppercase text-black">
+                <p>VISIT WEBSITE</p>
+                <img src="https://assets.website-files.com/6458c625291a94a195e6cf3a/64b636d7c440a74b4076b278_button-link.svg" alt="" className="inline-block" />
+              </a>
+              <a href="#" className="inline-block rounded-md bg-black px-6 py-3 text-center font-semibold text-white">View Project</a>
+            </div>
+          </div>
+        </div>
+        {/* <!--  Item --> */}
+        <div className="mb-12 inline-block border border-solid border-[#cdcdcd] md:mb-8 lg:mb-10">
+          <img src="https://assets.website-files.com/624380709031623bfe4aee60/6243807090316262904aee69_Placeholder%20Image%20-%20Landscape.svg" alt="" className="inline-block" />
+          <div className="px-5 py-8 sm:px-6">
+            <h5 className="mb-3 text-xl font-bold">Project name here</h5>
+            <p className="flex-col text-[#808080]">Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna</p>
+            <div className="mb-5 mt-6 flex flex-wrap gap-2 md:mb-6 lg:mb-8">
+              <div className="rounded-sm bg-[#d9d9d9] p-2 text-sm font-semibold uppercase text-[#636262]">
+                <p>WEB DESIGN</p>
+              </div>
+              <div className="rounded-sm bg-[#d9d9d9] p-2 text-sm font-semibold uppercase text-[#636262]">
+                <p>UI / UX</p>
+              </div>
+              <div className="rounded-sm bg-[#d9d9d9] p-2 text-sm font-semibold uppercase text-[#636262]">
+                <p>WEBFLOW</p>
+              </div>
+            </div>
+            <div className="flex flex-wrap items-center justify-between gap-4">
+              <a href="#" className="r flex max-w-full gap-2.5 text-sm font-bold uppercase text-black">
+                <p>VISIT WEBSITE</p>
+                <img src="https://assets.website-files.com/6458c625291a94a195e6cf3a/64b636d7c440a74b4076b278_button-link.svg" alt="" className="inline-block" />
+              </a>
+              <a href="#" className="inline-block rounded-md bg-black px-6 py-3 text-center font-semibold text-white">View Project</a>
+            </div>
+          </div>
+        </div>
+        {/* <!--  Item --> */}
+        <div className="mb-12 inline-block border border-solid border-[#cdcdcd] md:mb-8 lg:mb-10">
+          <img src="https://assets.website-files.com/624380709031623bfe4aee60/6243807090316203124aee66_placeholder-image.svg" alt="" className="inline-block" />
+          <div className="px-5 py-8 sm:px-6">
+            <h5 className="mb-3 text-xl font-bold">Project name here</h5>
+            <p className="flex-col text-[#808080]">Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna</p>
+            <div className="mb-5 mt-6 flex flex-wrap gap-2 md:mb-6 lg:mb-8">
+              <div className="rounded-sm bg-[#d9d9d9] p-2 text-sm font-semibold uppercase text-[#636262]">
+                <p>WEB DESIGN</p>
+              </div>
+              <div className="rounded-sm bg-[#d9d9d9] p-2 text-sm font-semibold uppercase text-[#636262]">
+                <p>UI / UX</p>
+              </div>
+              <div className="rounded-sm bg-[#d9d9d9] p-2 text-sm font-semibold uppercase text-[#636262]">
+                <p>WEBFLOW</p>
+              </div>
+            </div>
+            <div className="flex flex-wrap items-center justify-between gap-4">
+              <a href="#" className="r flex max-w-full gap-2.5 text-sm font-bold uppercase text-black">
+                <p>VISIT WEBSITE</p>
+                <img src="https://assets.website-files.com/6458c625291a94a195e6cf3a/64b636d7c440a74b4076b278_button-link.svg" alt="" className="inline-block" />
+              </a>
+              <a href="#" className="inline-block rounded-md bg-black px-6 py-3 text-center font-semibold text-white">View Project</a>
+            </div>
+          </div>
+        </div>
+        {/* <!--  Item --> */}
+        <div className="mb-12 inline-block border border-solid border-[#cdcdcd] md:mb-8 lg:mb-10">
+          <img src="https://assets.website-files.com/624380709031623bfe4aee60/6243807090316262904aee69_Placeholder%20Image%20-%20Landscape.svg" alt="" className="inline-block" />
+          <div className="px-5 py-8 sm:px-6">
+            <h5 className="mb-3 text-xl font-bold">Project name here</h5>
+            <p className="flex-col text-[#808080]">Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna</p>
+            <div className="mb-5 mt-6 flex flex-wrap gap-2 md:mb-6 lg:mb-8">
+              <div className="rounded-sm bg-[#d9d9d9] p-2 text-sm font-semibold uppercase text-[#636262]">
+                <p>WEB DESIGN</p>
+              </div>
+              <div className="rounded-sm bg-[#d9d9d9] p-2 text-sm font-semibold uppercase text-[#636262]">
+                <p>UI / UX</p>
+              </div>
+              <div className="rounded-sm bg-[#d9d9d9] p-2 text-sm font-semibold uppercase text-[#636262]">
+                <p>WEBFLOW</p>
+              </div>
+            </div>
+            <div className="flex flex-wrap items-center justify-between gap-4">
+              <a href="#" className="r flex max-w-full gap-2.5 text-sm font-bold uppercase text-black">
+                <p>VISIT WEBSITE</p>
+                <img src="https://assets.website-files.com/6458c625291a94a195e6cf3a/64b636d7c440a74b4076b278_button-link.svg" alt="" className="inline-block" />
+              </a>
+              <a href="#" className="inline-block rounded-md bg-black px-6 py-3 text-center font-semibold text-white">View Project</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
   )
 }
