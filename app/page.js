d1c2b3cb102f32c8579ea40310907c28215c442c
Damion Morgan <only4school5680@gmail.com>
2024-05-21 01:57:05 -0400

    oh the sweet realse of death

--- app/page.js
+++ app/page.js
@@ -154,6 +154,7 @@ export default function Home() {
       {
         open?(
           <>
+          <h1>Welcome Back {user?.username||user?.email} </h1>
           <h3 className="text-2xl font-bold mb-4 my-auto hover:underline" onClick={toggleOpen}>Active Tasks</h3>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
   <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 10.5V6.75a4.5 4.5 0 1 1 9 0v3.75M3.75 21.75h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H3.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
